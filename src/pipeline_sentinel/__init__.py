89355c49f78e90d0b0735211a5f8471f4a92d535
Chris Coffey <christopher.L.coffey@gmail.com>
2026-09-13 20:00:47 -0400

    Bump package version to v0.7.0

--- src/pipeline_sentinel/__init__.py
+++ src/pipeline_sentinel/__init__.py
@@ -3,4 +3,4 @@
 from .types import Alert, Detection, Event, FrameContext, FrameRecord, Track
 
 __all__ = ["Alert", "Detection", "Event", "FrameContext", "FrameRecord", "Track"]
-__version__ = "0.6.0"
+__version__ = "0.7.0"
